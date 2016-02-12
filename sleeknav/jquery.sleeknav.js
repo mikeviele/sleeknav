3956d3eab07556e6eac186e92f2b5b724c6bd29d
Mark Marzeotti <mark@axionmedialab.com>
2016-02-12 14:52:46 -0500

    added navPosition option

--- sleeknav/jquery.sleeknav.js
+++ sleeknav/jquery.sleeknav.js
@@ -10,12 +10,33 @@
 
 (function ( $ ) {
 
-	$.fn.sleeknav = function() {
+	$.fn.sleeknav = function( options ) {
+
+		var defaults = {
+			navPosition: 'fixed'
+	    };
+
+		var settings = $.extend( {}, defaults, options );
 
 	    var sleekul = this.html();
 		this.children('ul').remove();
 		this.prepend('<div class="sleekburger"><a href="#"></a></div><div class="sleeknav"><div class="sleek-overlay"><div class="sleek-background"></div><div class="sleek-overlay-outer"><div class="sleek-overlay-inner">' + sleekul + '</div></div></div></div>');
 		this.addClass('sleek');
+
+		switch ( settings.navPosition ) {
+
+		    case 'fixed':
+		        this.addClass('sleek-fixed');
+		        break;
+
+		    case 'relative':
+		        this.addClass('sleek-relative');
+		        break;
+
+		    default:
+		        this.addClass('sleek-fixed');
+
+		}
 
 		//take all parents and append them above their children
 		var parent;
@@ -82,77 +103,6 @@
 			}
 		});
 
-
 	};
 
 }( jQuery ));
-
-/*
-jQuery(document).ready(function($) {
-
-	//take all parents and append them above their children
-	var parent;
-	$('.sleek ul ul').each(function() {
-		//store contents of parent li
-		parent = $(this).parent('li').html();
-		//prepend ul with its parent link
-		$(this).prepend('<li class="menu-parent">' + parent + '</li>');
-		//remove ul from contents of parent li
-		$(this).children('li').first().children('ul').remove();
-	});
-
-	//if fixed positioning add padding to sleeknavs container
-	if ($('.sleek-fixed').length > 0) {
-		var sleekHeight = parseInt($('.sleek-fixed').outerHeight());
-		var sleekPadding = parseInt($('.sleek-fixed').parent().css('padding-top'));
-		$('.sleek-fixed').parent().css('padding-top', sleekPadding + sleekHeight);
-	}
-
-	//controlling functions
-	var flag;
-	$('.sleekburger a').click(function(e) {
-		e.preventDefault();
-		$('.sleeknav ul ul').slideUp(300);
-		$('.sleeknav ul li a').css('color', 'rgba(255, 255, 255, 1)');
-		$('.sleekburger').toggleClass('sleek-close');
-		$('.sleeknav').fadeToggle();
-		$('.sleeknav .sleek-overlay-outer').height($('.sleek-overlay-outer .sleek-overlay-inner').height());
-	});
-	$('.sleeknav ul li a').click(function(e) {
-		//if this has children
-		if ($(this).parent().children('ul').length > 0) {
-			//has children
-			e.preventDefault();
-			if (!$(this).parent().children('ul').is(':visible')) {
-				//children are not visible
-				$(this).parent().children('ul').slideDown(300);
-				$(this).parent().siblings().children('ul').slideUp(300);
-				$(this).closest('ul').children('li').children('a').css('color', 'rgba(255, 255, 255, 0.2)');
-				flag = 0;
-				setInterval(function() {
-					flag++;
-					$('.sleeknav .sleek-overlay-outer').height($('.sleeknav .sleek-overlay-inner').height());
-					if (flag == 30) {
-						return;
-					}
-				}, 10);
-				return;
-			}
-			if ($(this).parent().children('ul').is(':visible')) {
-				//children are currently visible
-				$(this).parent().children('ul').slideUp(300);
-				$(this).closest('ul').children('li').children('a').css('color', 'rgba(255, 255, 255, 1)');
-				flag = 0;
-				setInterval(function() {
-					flag++;
-					$('.sleeknav .sleek-overlay-outer').height($('.sleeknav .sleek-overlay-inner').height());
-					if (flag == 30) {
-						return;
-					}
-				}, 10);
-				return;
-			}
-		}
-	});
-});
-*/
